4f5cb0d7ceaf49415bbb33a7e69f4e66b772c76c
Andriy Tevelyev <info@innerbytes.com>
2025-01-04 18:09:32 +0100

    Reorganize the solution: add Ida files to a separate project

---
+++ Ida/src/ElasticBuffer.cpp
@@ -0,0 +1,48 @@
+#include "ElasticBuffer.h"
+#include <stdexcept>
+#include <cmath>
+#include <iostream>
+
+using namespace std;
+
+namespace Ida {
+    // TODO - setup auto-format in vscode
+    ElasticBuffer::ElasticBuffer(size_t initialSize, double growthRate)
+        : currentCapacity(initialSize), growthRate(growthRate) {
+        if (growthRate <= 1) {
+            throw invalid_argument("Growth rate must be greater than 1.0.");
+        }
+        if (initialSize > 0) {
+            buffer.reserve(initialSize);
+        }
+    }
+
+    void ElasticBuffer::ensureCapacity(size_t requiredSize) {
+        if (!requiredSize)
+        {
+            return;
+        }
+
+        if (!buffer.capacity()) {
+            currentCapacity = requiredSize * 2;
+            buffer.reserve(currentCapacity);
+            // TODO - remove debug messages
+            std::cout << "Buffer initially reallocated to " << currentCapacity << " bytes." << std::endl;
+        }
+
+        if (requiredSize > currentCapacity) {
+            while (currentCapacity < requiredSize) {
+                currentCapacity = static_cast<size_t>(std::ceil(currentCapacity * growthRate));
+            }
+            buffer.reserve(currentCapacity);
+            std::cout << "Buffer reallocated to " << currentCapacity << " bytes." << std::endl;
+        }
+        buffer.resize(requiredSize);
+    }
+
+    // Access the raw buffer pointer
+    char* ElasticBuffer::getBuffer() {
+        return buffer.data();
+    }
+}
+
---
+++ Ida/src/ElasticBuffer.h
@@ -0,0 +1,21 @@
+#pragma once
+
+#include <vector>
+#include <cstddef>
+
+namespace Ida {
+    class ElasticBuffer {
+    public:
+        ElasticBuffer(size_t initialSize = 0, double growthRate = 2);
+
+        void ensureCapacity(size_t requiredSize);
+
+        char* getBuffer();
+
+    private:
+        std::vector<char> buffer;
+        size_t currentCapacity;
+        double growthRate;
+    };
+}
+
